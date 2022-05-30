ca666e3c8e97fb1244a937401ee9d8a54973e72e
Matthew Hayes <matthewha853@gmail.com>
2022-05-30 12:59:39 -0700

    Add CORS config

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -12,6 +12,19 @@ public func configure(_ app: Application) throws {
     
     try app.autoMigrate().wait()
     
+    configureCORS(app)
+    
     // register routes
     try routes(app)
+}
+
+private func configureCORS(_ app: Application) {
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    
+    app.middleware.use(cors, at: .beginning)
 }
